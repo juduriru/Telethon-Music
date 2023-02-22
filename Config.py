e35a3473423ec2cf5eaf316969c095b72b909c96
juduriru <54899877+juduriru@users.noreply.github.com>
2023-02-22 10:57:11 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6213538"))
-    API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
+    API_ID = int(os.environ.get("APP_ID", "20515805"))
+    API_HASH = os.environ.get("API_HASH", "331d6eab3e285880282252b854e20975")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6059266175:AAHveGHVxwxOl-739kGwkdY9brGB9A9BvLw")
     STRING_SESSION = os.environ.get("STRING_SESSION", "1BVtsOJIBuyVgDY04ry3Z049w0gnMzkdQBDGFjmL9ykkD4vF0DgGPNI2U826RhvdZt8d38Gcu4YSnxtHxOJWwz1-8ULlRvMSuFDioFaOhansuuLrk7zDMFnZeRDYwM-bk9dYloyc-7FpKkIhdtIEonxiGRl5Twwi3y6rWGTYKLFavVu7TWY3EFUkFyFYmvv4tj7u6VJNKJM995werhHEhRSf6qau4LkW7lfaCYDIZkWo0hQZcI6_W9y0C2fw4162iJAj5EanGZ_MLj6q38tGqlc8R4LNelKxA8Dbb6sfBxptYwcdU0PdDyk8_S__2xDoPTxGGNTIxWDXT-uBHpqSH_gNhbvCQlFA=")
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
